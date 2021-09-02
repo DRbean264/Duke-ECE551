a941252b124f59491a999b4f05e28d82efbb9b7a
yd160 <yd160@duke.edu>
2021-09-02 00:52:34 -0400

    Complete the rectangle.c

--- 021_rectangle/rectangle.c
+++ 021_rectangle/rectangle.c
@@ -16,15 +16,55 @@ int max (int a, int b) {
 }
 
 //Declare your rectangle structure here!
-
+struct _rectangle {
+    int x;
+    int y;
+    int width;
+    int height;
+};
+typedef struct _rectangle rectangle;
 
 rectangle canonicalize(rectangle r) {
-  //WRITE THIS FUNCTION
-  return r;
+    //WRITE THIS FUNCTION
+    if (r.width < 0) {
+        r.x += r.width;
+        r.width = -r.width;
+    }
+    if (r.height < 0) {
+        r.y += r.height;
+        r.height = -r.height;
+    }
+    return r;
 }
 rectangle intersection(rectangle r1, rectangle r2) {
-  //WRITE THIS FUNCTION
-  return r1;
+    //WRITE THIS FUNCTION
+    r1 = canonicalize(r1);
+    r2 = canonicalize(r2);
+    rectangle rIntersect;
+
+    rIntersect.x = max(r1.x, r2.x);
+    rIntersect.y = max(r1.y, r2.y);
+
+    int empty = 0;
+    if (r1.x > r2.x) {
+        rIntersect.width = min(r2.x + r2.width - rIntersect.x, r1.width);  
+    } else {
+        rIntersect.width = min(r1.x + r1.width - rIntersect.x, r2.width);
+    }
+    if (rIntersect.width < 0) empty = 1;
+    
+    if (r1.y > r2.y) {
+        rIntersect.height = min(r2.y + r2.height - rIntersect.y, r1.height);
+    } else {
+        rIntersect.height = min(r1.y + r1.height - rIntersect.y, r2.height);
+    }
+    if (rIntersect.height < 0) empty = 1;    
+
+    if (empty) {
+        rIntersect.width = 0;
+        rIntersect.height = 0;
+    }
+    return rIntersect;
 }
 
 //You should not need to modify any code below this line

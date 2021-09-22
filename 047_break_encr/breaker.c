741b3cdcd8ffb2cf5bd6bab4a1f7674a060357ca
yd160 <yd160@duke.edu>
2021-09-22 00:29:07 -0400

    finish the breaker.c code

---
+++ 047_break_encr/breaker.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        fprintf(stderr, "The command line arguments is of wrong number!");
+        exit(EXIT_FAILURE);
+    }
+    //  open the file
+    FILE *f = fopen(argv[1], "r");
+    if (f == NULL) {
+         fprintf(stderr, "Fail to open the requested file!");
+         exit(EXIT_FAILURE);
+    }
+
+    int freq[26] = {0};
+    int c;
+    while ((c = fgetc(f)) != EOF) {
+        if (isalpha(c)) {
+            ++freq[c - 'a'];
+        }
+    }
+
+    //  find the most frequent char
+    size_t maxFreqIndex = 0;
+    int maxFreq = freq[0];
+    for (size_t i = 1; i < 26; ++i) {
+        if (freq[i] > maxFreq) {
+            maxFreqIndex = i;
+            maxFreq = freq[i];
+        }
+    }
+
+    // printf("%c\n", 'a' + (int)maxFreqIndex);
+    int key;
+    if (maxFreqIndex > 4) key = maxFreqIndex - 4;
+    else key = 26 - (4 - maxFreqIndex);
+
+    printf("%d\n", key);
+    return EXIT_SUCCESS;
+}

3692d3eba598da3fcd8f3d40a11c351f0259916a
yd160 <yd160@duke.edu>
2021-09-25 00:21:09 -0400

    finish the running average function

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -10,10 +10,10 @@ country_t parseLine(char * line) {
     //  find comma
     char *commaPos = strchr(line, ',');
     if (commaPos == NULL) {  //  if , is not found
-        fprintf(stderr, "Format error: there's no comma");
+        fprintf(stderr, "Format error: there's no comma\n");
         exit(EXIT_FAILURE);
     } else if (commaPos - line > 63) {  //  name too long
-        fprintf(stderr, "Format error: the name is too long");
+        fprintf(stderr, "Format error: the name is too long\n");
         exit(EXIT_FAILURE);    
     }
     //  store the name
@@ -26,12 +26,12 @@ country_t parseLine(char * line) {
     //  check the format of the population string
     char *tmp = commaPos + 1;
     if (*tmp == '\n') {  //  population string is empty
-        fprintf(stderr, "Format error: population is not given");
+        fprintf(stderr, "Format error: population is not given\n");
         exit(EXIT_FAILURE);
     }
     while (*tmp != '\n') {
         if (*tmp < '0' || *tmp > '9') {  //  if meet a non-number
-            fprintf(stderr, "Format error: population contains other character other than number");
+            fprintf(stderr, "Format error: population contains other character other than number\n");
             exit(EXIT_FAILURE);    
         }
         ++tmp;
@@ -44,6 +44,22 @@ country_t parseLine(char * line) {
 
 void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
     //WRITE ME
+    if (n_days < 7) {  //  not enough days
+        fprintf(stderr, "Running average error: the days are less than 7\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    int sum_seven_day = 0;
+    for (int i = 0; i < 7; ++i) {
+        sum_seven_day += data[i];
+    }
+    avg[0] = sum_seven_day / 7.0;
+
+    for (size_t i = 1; i < n_days - 6; ++i) {
+        sum_seven_day -= data[i - 1];
+        sum_seven_day += data[i + 6];
+        avg[i] = sum_seven_day / 7.0;
+    }
 }
 
 void calcCumulative(unsigned * data, size_t n_days, uint64_t pop, double * cum) {

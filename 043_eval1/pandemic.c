e7b79e032c5ad86bef5dec1b64284d9fb2b64ee2
yd160 <yd160@duke.edu>
2021-09-26 00:25:40 -0400

    debug

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -32,9 +32,12 @@ country_t parseLine(char * line) {
         exit(EXIT_FAILURE);
     }
     //  skip the white space directly after the comma
-    while (*(tmp++) == ' ');
+    while (*tmp == ' ') {
+        ++tmp;
+    }
     
     if ((*tmp < '0' || *tmp > '9') && *tmp != '+' && *tmp != '-') {  //  if the character directly after the comma is not a number
+        printf("%c\n", *tmp);
         fprintf(stderr, "Format error: population contains other character other than number\n");
         exit(EXIT_FAILURE);
     } 

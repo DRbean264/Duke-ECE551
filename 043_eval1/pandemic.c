6ca1cf12d39c49a11bcd1abfed73db1f41fa0761
yd160 <yd160@duke.edu>
2021-09-29 00:53:51 -0400

    final version, add comments, abstract functions

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -17,24 +17,10 @@ void countryNameFormatChecking(const char *line, const char *commaPos) {
     }
 }
 
-country_t parseLine(char * line) {
-    //WRITE ME
-    country_t ans;
-    
-    //  find comma
-    char *commaPos = strchr(line, ',');
-    // format checking
-    countryNameFormatChecking(line, commaPos);
-    //  store the name
-    int i;
-    for (i = 0; (line + i) != commaPos; ++i) {
-        ans.name[i] = line[i];
-    }
-    ans.name[i] = '\0';
-
-    //  check the format of the population string
-    char *tmp = commaPos + 1;
-    if (*tmp == '\n') {  //  population string is empty
+//  check the format of the population
+void populationFormatChecking(const char *commaPos) {
+    const char *tmp = commaPos + 1;
+    if (*tmp == '\n') {  //  population string is empty, error
         fprintf(stderr, "Format error: population is not given\n");
         exit(EXIT_FAILURE);
     }
@@ -45,19 +31,40 @@ country_t parseLine(char * line) {
     
     if (*tmp < '0' || *tmp > '9') {  //  if the character directly after the comma is not a number
         if (*tmp == '-' || *tmp == '+') {  //  if it's the +/-
-            if (*(tmp + 1) < '0' || *(tmp + 1) > '9') {
+            if (*(tmp + 1) < '0' || *(tmp + 1) > '9') {  //  if the charater after the +/- is still not a number, error
                 fprintf(stderr, "Format error: population contains other character other than number after the sign character\n");
                 exit(EXIT_FAILURE);
             }
-        } else {
+        } else {  //  if the charater is not even a +/-, error
             fprintf(stderr, "Format error: population begins with other character other than number and sign character\n");
             exit(EXIT_FAILURE);   
         }
-    } 
-    //  pass format checking, store the population
+    }
+}
+
+country_t parseLine(char * line) {
+    //WRITE ME
+    country_t ans;
+    
+    //  find comma
+    char *commaPos = strchr(line, ',');
+    //  format checking
+    countryNameFormatChecking(line, commaPos);
+
+    //  store the country name
+    int i;
+    for (i = 0; (line + i) != commaPos; ++i) {
+        ans.name[i] = line[i];
+    }
+    ans.name[i] = '\0';
+
+    //  check the format of the population string
+    populationFormatChecking(commaPos);
+    
+    //  store the population
     char *leftover;    
     unsigned long long tmpPop = strtoull(commaPos + 1, &leftover, 10);
-    if (tmpPop == ULONG_MAX && errno == ERANGE) {
+    if (tmpPop == ULLONG_MAX && errno == ERANGE) {  //  if overflow the ULLONG_MAX, error
         fprintf(stderr, "Population error: the population is too large to be fit into the variable\n");
         exit(EXIT_FAILURE);
     }
@@ -67,18 +74,17 @@ country_t parseLine(char * line) {
 
 void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
     //WRITE ME
+    /* if less than seven days, do nothing */
     if (n_days < 7) return;
-    
+
     int sum_seven_day = 0;
-    for (size_t i = 0; i < (n_days < 7 ? n_days : 7); ++i) {
+    /* calculate the first element */
+    for (size_t i = 0; i < 7; ++i) {
         sum_seven_day += data[i];
     }
-    if (n_days < 7) {  //  the array avg only has one element
-        avg[0] = sum_seven_day * 1.0 / n_days;
-        return;
-    }    
     avg[0] = sum_seven_day / 7.0;
 
+    /* calculate the rest element */
     for (size_t i = 1; i < n_days - 6; ++i) {
         sum_seven_day -= data[i - 1];
         sum_seven_day += data[i + 6];
@@ -88,15 +94,32 @@ void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
 
 void calcCumulative(unsigned * data, size_t n_days, uint64_t pop, double * cum) {
     //WRITE ME
+    /* if the number of days is zero, do nothing */
     if (n_days == 0) return;
     
-    //  calculate the first one
+    /* calculate the first element */
     uint64_t cumulative = data[0];
     cum[0] = cumulative * 100000.0 / pop;
 
+    /* calculate the rest elements */
     for (size_t i = 1; i < n_days; ++i) {
         cumulative += data[i];
         cum[i] = cumulative * 100000.0 / pop;
+    }
+}
+
+void findMax(size_t *maxCountryIndex, size_t *maxDayIndex, int *isTie, unsigned **data, size_t n_countries, size_t n_days) {
+    *isTie = 0;
+    for (size_t i = 0; i < n_countries; ++i) {
+        for (size_t j = 0; j < n_days; ++j) {
+            if (data[i][j] > data[*maxCountryIndex][*maxDayIndex]) {
+                *maxCountryIndex = i;
+                *maxDayIndex = j;
+                *isTie = 0;
+            } else if (data[i][j] == data[*maxCountryIndex][*maxDayIndex]) {
+                *isTie = 1;
+            }
+        }        
     }
 }
 
@@ -105,23 +128,19 @@ void printCountryWithMax(country_t * countries,
                          unsigned ** data,
                          size_t n_days) {
     //WRITE ME
+    /* if one of the number is 0, do nothing */
     if (n_countries == 0 || n_days == 0) return;
-    
+
+    /* record the location of the max element */
     size_t maxCountryIndex = 0;
     size_t maxDayIndex = 0;
+    /* indicate the tie condition */
     int isTie = 0;
-    for (size_t i = 0; i < n_countries; ++i) {
-        for (size_t j = 0; j < n_days; ++j) {
-            if (data[i][j] > data[maxCountryIndex][maxDayIndex]) {
-                maxCountryIndex = i;
-                maxDayIndex = j;
-                isTie = 0;
-            } else if (data[i][j] == data[maxCountryIndex][maxDayIndex]) {
-                isTie = 1;
-            }
-        }        
-    }
-    if (isTie) {  //  if there is a tie
+    /* find the max of the matrix */
+    findMax(&maxCountryIndex, &maxDayIndex, &isTie, data, n_countries, n_days);
+
+    /* if there is a tie */
+    if (isTie) {
         printf("There is a tie between at least two countries\n");
     } else {
         printf("%s has the most daily cases with %u\n", countries[maxCountryIndex].name, data[maxCountryIndex][maxDayIndex]);

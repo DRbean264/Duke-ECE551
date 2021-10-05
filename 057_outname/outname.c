fa4305ab0ba81e11adbeae12db5077f3f32fc66e
yd160 <yd160@duke.edu>
2021-10-05 16:00:00 -0400

    finish the output name function

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -4,5 +4,16 @@
 #include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
+    //WRITE ME
+    char *outputName = NULL;
+    size_t newLen = strlen(inputName) + 8;
+    outputName = malloc(newLen);
+    for (size_t i = 0; i < newLen; ++i) {
+        outputName[i] = '\0';
+    }
+
+    outputName = strcat(outputName, inputName);
+    outputName = strcat(outputName, ".counts");
+
+    return outputName;
 }

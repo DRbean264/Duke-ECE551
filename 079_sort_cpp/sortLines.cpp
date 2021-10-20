6b47e04ad7e15e617c763c97f444cdb0fe747bde
yd160 <yd160@duke.edu>
2021-10-20 19:22:55 -0400

    convert the sortstring into c++

---
+++ 079_sort_cpp/sortLines.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <algorithm>
+using namespace std;
+
+template<typename T>
+void sortAndPrintFromFile(T &s) {
+    vector<string> lines;
+    string line;
+    while (getline(s, line).eof() != true) {
+        lines.push_back(line);
+    }    
+    
+    sort(lines.begin(), lines.end());
+    // print the strings
+    for (size_t i = 0; i < lines.size(); ++i) {
+        cout << lines[i] << endl;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1) {            // read from stdin
+        sortAndPrintFromFile<istream>(cin);
+    } else if (argc >= 1){      // if have files
+        for (size_t i = 1; i < (size_t)argc; ++i) {
+            ifstream f(argv[i]);
+            sortAndPrintFromFile<ifstream>(f);
+        }
+    }
+    return 0;
+}

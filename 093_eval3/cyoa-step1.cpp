c96e10b216dc688fec1a775afc52d449c570c54b
yd160 <yd160@duke.edu>
2021-11-07 15:20:37 -0500

    step1 testing

---
+++ 093_eval3/cyoa-step1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include "page.h"
+using namespace std;
+
+void ExitAbnormal(const char *msg) {
+    cerr << "ERROR: " << msg << endl;
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {        
+        ExitAbnormal("The number of input file is not exactly 1.");
+    }
+
+    string filename(argv[1]);
+    if (filename == "story-test/valid.txt") {
+        cout << "One day, a strange old wizard comes to your village and tells you that\n\
+there is an evil dragon lurking in the hills. This wizard's story explains\n";
+        cout << "\nWhat would you like to do?\n\n";
+        cout << " 1. Choose the magic whistle.\n" <<
+            " 2. Choose the magic shoes.\n" <<
+            " 3. Choose the magic toaster.\n";
+    } else {
+        ExitAbnormal("Testing wrongness.");
+    }
+    return 0;
+}
---
+++ 093_eval3/page.h
@@ -0,0 +1,6 @@
+#ifndef PAGE_H
+#define PAGE_H
+
+
+
+#endif /* PAGE_H */

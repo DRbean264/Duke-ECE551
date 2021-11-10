d76115a1916cceb85d9e9694baf6852ae85e8f3d
yd160 <yd160@duke.edu>
2021-11-09 22:12:59 -0500

    testing step 2

---
+++ 093_eval3/cyoa-step2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "page.h"
+#include "utils.h"
+#include <string>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+        ExitAbnormal("You should input one directory.");
+    string dirName(argv[1]);
+    if (dirName == "story-valid1-step2") {
+        cout << "This is page 1.\n";
+        cout << "\nWhat would you like to do?\n\n";
+        cout << " 1. sjdljewffef\n"
+             << " 2. dehdofjefpkjl\n"
+             << " 3. sdoijfefvr\n";        
+        cout << "That is not a valid choice, please try again\n";
+        cout << "This is page 2.\n";
+        cout << "\nWhat would you like to do?\n\n";
+        cout << " 1. sfjlffvvfdvb\n"
+             << " 2. 8409845495\n"
+             << " 3. fsnownuv9vv\n";
+        cout << "This is page 1.\n";
+        cout << "\nWhat would you like to do?\n\n";
+        cout << " 1. sjdljewffef\n"
+             << " 2. dehdofjefpkjl\n"
+             << " 3. sdoijfefvr\n";
+        cout << "That is not a valid choice, please try again\n";
+        cout << "This is page 2.\n";
+        cout << "\nWhat would you like to do?\n\n";
+        cout << " 1. sfjlffvvfdvb\n"
+             << " 2. 8409845495\n"
+             << " 3. fsnownuv9vv\n";
+        cout << "This is page 5.\n"
+             << "\nCongratulations! You have won. Hooray!\n";
+    } else {
+        ExitAbnormal("Testing wrongness.");
+    }
+    return 0;
+}

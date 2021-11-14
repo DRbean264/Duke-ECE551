b929af687bcc75af6371a9a40ee8920309b4ca8f
yd160 <yd160@duke.edu>
2021-11-14 18:14:14 -0500

    finsh

--- 102_huff_freq/readFreq.cpp
+++ 102_huff_freq/readFreq.cpp
@@ -1,24 +1,38 @@
 #include <stdio.h>
 #include <cstdlib>
+#include <fstream>
 #include "readFreq.h"
 
 void printSym(std::ostream & s, unsigned sym) {
-  if (sym > 256 ) {
-    s << "INV";
-  }
-  else if (sym == 256) {
-    s << "EOF";
-  }
-  else if (isprint(sym)) {
-    char c = sym;
-    s << "'" << c << "'";
-  }
-  else {
-    std::streamsize w = s.width(3);
-    s << std::hex << sym << std::dec;
-    s.width(w);
-  }
+    if (sym > 256 ) {
+        s << "INV";
+    }
+    else if (sym == 256) {
+        s << "EOF";
+    }
+    else if (isprint(sym)) {
+        char c = sym;
+        s << "'" << c << "'";
+    }
+    else {
+        std::streamsize w = s.width(3);
+        s << std::hex << sym << std::dec;
+        s.width(w);
+    }
 }
 uint64_t * readFrequencies(const char * fname) {
-  //WRITE ME!
+    std::ifstream inFile(fname, std::ifstream::in);
+    if (!inFile.good()) {
+        std::cerr << "Can not open the file.\n";
+        exit(EXIT_FAILURE);
+    }        
+    uint64_t *freq = new uint64_t[257]();
+
+    int ch;
+    while ((ch = inFile.get()) != -1) {
+        freq[ch] += 1;
+    }
+    freq[256] = 1;
+
+    return freq;
 }

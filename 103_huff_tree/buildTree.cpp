b1f2e7b3f05eccd8d2c23775281d8a91faf18b15
yd160 <yd160@duke.edu>
2021-11-14 18:37:32 -0500

    finish

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -1,5 +1,24 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
-  //WRITE ME!
+Node * buildTree(uint64_t * counts) {
+    if (counts == NULL)
+        return NULL;
+    
+    priority_queue_t pq;
+    for (size_t i = 0; i < 257; ++i) {
+        if (counts[i] != 0) {
+            pq.push(new Node(i, counts[i]));
+        }
+    }
+
+    while (pq.size() != 1) {
+        // fetch two elements
+        Node *first = pq.top();
+        pq.pop();
+        Node *second = pq.top();
+        pq.pop();
+        Node *comb = new Node(first, second);
+        pq.push(comb);
+    }
+    return pq.top();
 }  

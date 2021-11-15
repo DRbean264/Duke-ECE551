7a7b063c24892eb235bf4fe33565bd7145bb0289
yd160 <yd160@duke.edu>
2021-11-14 20:22:36 -0500

    test

--- 105_huff_all/compress.cpp
+++ 105_huff_all/compress.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <map>
+#include <fstream>
 #include <queue>
 #include <assert.h>
 #include <functional>
@@ -25,33 +26,48 @@ void writeHeader(BitFileWriter * bfw, const std::map<unsigned,BitString> &theMap
 }
 
 void writeCompressedOutput(const char* inFile,
-			   const char *outFile,
-			   const std::map<unsigned,BitString> &theMap ){
-  BitFileWriter bfw(outFile);
-  writeHeader(&bfw,theMap);
+                           const char *outFile,
+                           const std::map<unsigned,BitString> &theMap ){
+    BitFileWriter bfw(outFile);
+    writeHeader(&bfw,theMap);
 
-  //WRITE YOUR CODE HERE!
-  //open the input file for reading
-
-  //You need to read the input file, lookup the characters in the map,
-  //and write the proper bit string with the BitFileWriter
-
-  //dont forget to lookup 256 for the EOF marker, and write it out.
-
-  //BitFileWriter will close the output file in its destructor
-  //but you probably need to close your input file.
+    //WRITE YOUR CODE HERE!
+    //open the input file for reading
+    std::ifstream f(inFile, std::ifstream::in);
+    //You need to read the input file, lookup the characters in the map,
+    //and write the proper bit string with the BitFileWriter
+    int ch;
+    while ((ch = f.get()) != -1) {
+        assert(theMap.find(ch) != theMap.end());
+        bfw.writeBitString(theMap.find(ch)->second);
+    }
+    //dont forget to lookup 256 for the EOF marker, and write it out.
+    bfw.writeBitString(theMap.find(256)->second);
+    //BitFileWriter will close the output file in its destructor
+    //but you probably need to close your input file.
 }
 
 int main(int argc, char ** argv) {
-  if (argc != 3) {
-    fprintf(stderr,"Usage: compress input output\n");
-    return EXIT_FAILURE;
-  }
-  //WRITE YOUR CODE HERE
-  //Implement main
-  //hint 1: most of the work is already done. 
-  //hint 2: you can look at the main from the previous tester for 90% of this
+    if (argc != 3) {
+        fprintf(stderr,"Usage: compress input output\n");
+        return EXIT_FAILURE;
+    }
+    //WRITE YOUR CODE HERE
+    //Implement main
+    //hint 1: most of the work is already done. 
+    //hint 2: you can look at the main from the previous tester for 90% of this
+    const char *inFile = argv[1];
+    const char *outFile = argv[2];
+    uint64_t *counts = readFrequencies(inFile);
+    assert(counts != NULL);
+    Node *tree = buildTree(counts);
+    delete[] counts;
+    std::map<unsigned,BitString> theMap;
+    BitString empty;
+    tree->buildMap(empty, theMap);
 
+    writeCompressedOutput(inFile, outFile, theMap);   
+    delete tree;
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
